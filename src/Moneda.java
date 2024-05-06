d5e3fd8a1b847da27524df9e0808208c102634fd
Tonatiuh <tylhuicatzip1700@alumno.ipn.mx>
2024-05-06 15:20:12 -0600

    Modificación de archivo moneda

--- src/Moneda.java
+++ src/Moneda.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public record Moneda() {
+public record Moneda(String base_code,
+                     String target_code,
+                     float conversion_rate,
+                     float conversion_result) {
+    @Override
+    public String toString() {
+        return ".\nValor de conversión: $" + conversion_rate +
+                ".\nResultado de la conversión: $" + conversion_result;
+    }
 }
